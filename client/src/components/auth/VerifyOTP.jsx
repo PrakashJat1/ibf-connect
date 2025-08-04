52fc316a7445ecf6243a67c3ae1a85b7a338359a
Prakash Jat <prakashjatt966@gmail.com>
2025-08-05 00:22:36 +0530

    Initial clean commit

---
+++ client/src/components/auth/VerifyOTP.jsx
@@ -0,0 +1,74 @@
+import FormWrapper from "@/components/common/Form/FormWrapper";
+import InputField from "@/components/common/Form/InputField";
+import Button from "@/components/common/UI/Button";
+import authService from "@/services/authService";
+import yupSchemas from "@/utils/yupSchemas";
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+const VerifyOTP = () => {
+  const location = useLocation();
+  const userData = location.state?.userData;
+  const navigate = useNavigate();
+
+  const handleSubmit = async (data) => {
+    const newData = {
+      email: userData.email,
+      otp: data.otp,
+    };
+
+    try {
+      await authService.verifyOTP(newData);
+      navigate('/login');
+      toast.success("Registered successfully...!😊");
+
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        toast.error(" Invalid or expired OTP");
+      } else {
+        toast.error("Something went wrong 😓");
+      }
+    }
+  };
+
+  const resendOTP = async () => {
+    const newData = {
+      fullName: userData.fullName,
+      email: userData.email,
+    };
+
+    let response = null;
+    try {
+      response = await authService.resendOTP(newData);
+
+      toast.success(response.data.message);
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        toast.error(response.data.message);
+      } else {
+        toast.error("Something went wrong 😓");
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="container mt-5">
+        <h2>OTP sent to {userData.email}</h2>
+
+        <FormWrapper
+          defaultValues={{ otp: "" }}
+          schema={yupSchemas.verifyOTPSchema}
+          onSubmit={handleSubmit}
+        >
+          <InputField name="otp" type="text" placeholder="Enter OTP" />
+          <Button type="submit" />
+          <Button onClick={resendOTP} label="Resend OTP" />
+        </FormWrapper>
+      </div>
+    </>
+  );
+};
+
+export default VerifyOTP;
---
+++ client/src/components/dashboard/ITEPApplicantDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ITEPApplicantDashboard = () => {
+  return (
+    <div>ITEPApplicantDashboard</div>
+  )
+}
+
+export default ITEPApplicantDashboard
